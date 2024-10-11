99ffb01189429da5b78cdffbb9b86ea242a1d348
Francis Kornu <fkay0450@gmail.com>
2024-10-11 10:36:38 +0000

    refact: calendly url changed

--- src/landing.jsx
+++ src/landing.jsx
@@ -224,7 +224,7 @@ export default function LandingPage(){
             <h1 className='section-header section-header-alt'>Visit our showrooms</h1>
             <p className='section-p section-header-alt'>Schedule an appointment with us today by selecting a convenient time, and our team will confirm your booking promptly.</p>
             
-            <a target="blank" href="https://calendly.com/thekid0450/book-consult" className='flex flex-row gap-4 button-green-primary justify-between mt-8'>
+            <a target="blank" href="https://calendly.com/invataafrica-info/30min" className='flex flex-row gap-4 button-green-primary justify-between mt-8'>
               <h1>Book appointment now</h1>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
@@ -249,7 +249,7 @@ export default function LandingPage(){
             </div>
             <h1 className='section-header section-header-alt'>Transform-MDS has the latest and highest-quality products.</h1>
             <p className='section-p section-header-alt'>Schedule an appointment with us today by selecting a convenient time, and our team will confirm your booking promptly.</p>
-            <a target="blank" href="https://calendly.com/thekid0450/book-consult" className='flex flex-row gap-4 button-green-primary justify-between mt-8'>
+            <a target="blank" href="https://calendly.com/invataafrica-info/30min" className='flex flex-row gap-4 button-green-primary justify-between mt-8'>
               <h1>Book appointment now</h1>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
@@ -296,7 +296,7 @@ export default function LandingPage(){
                 </div>
               </div>
               <div className="booking-button-block">
-                <a target="blank" href="https://calendly.com/thekid0450/book-consult"  className="booking-button w-inline-block">
+                <a target="blank" href="https://calendly.com/invataafrica-info/30min"  className="booking-button w-inline-block">
                   <div className="w-full">Booking Now</div>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
